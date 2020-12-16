7d30bd7c2593b0344a91777ebf62903a0b2722c1
Daniel Motz <herr.motz.os@gmail.com>
2020-12-16 18:06:31 +0100

    rework, because i did the task wrong

--- 5/berechnung_pi.c
+++ 5/berechnung_pi.c
@@ -3,33 +3,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 
 int main(void) {
-    printf("Geben Sie an, mit welcher Genauigkeit Pi berechnet werden soll (Angabe als Kommazahl):\n");
+    printf("Geben Sie an, bis zu welchem Delta zwischen n-1 und n Pi berechnet werden soll:\n");
     float precisionInput;
     scanf("%f", &precisionInput);
     precisionInput = fabsf(precisionInput);
 
-    float pi = 0.0f;
+    double pi = 0;
+    double old_pi = DBL_MAX;
     float Vorzeichen;
-    int i = 0;
+    int n = 0;
 
-    while (precisionInput < 1.0f) {
-
-        if (i++ % 2 == 0)
+    while (fabs(old_pi - pi) > precisionInput) {
+        if (n % 2 == 0)
             Vorzeichen = 1.0f;
         else
             Vorzeichen = -1.0f;
-
-        pi += Vorzeichen * (1.0f/(2.0f * (i-1) + 1.0f));
-        precisionInput *= 10;
+        old_pi = pi;
+        pi += 4.0 * Vorzeichen * (1.0 / (2.0 * n + 1.0));
+        n++;
     }
 
-    pi *= 4;
-
-    printf("berechnetes Pi: %f\n", pi);
-    printf("math.h Pi: %f\n", M_PI);
-    printf("Abweichung: %f\n", fabsf(pi - M_PI));
+    printf("berechnetes Pi: %.*f\n", DECIMAL_DIG, pi);
+    printf("Durchlaeufe: %d\n", n);
+    printf("\nmath.h Pi: %.*f\n", DECIMAL_DIG, M_PI);
+    printf("Abweichung: %f\n", fabs(pi - M_PI));
 
     return EXIT_SUCCESS;
 }
